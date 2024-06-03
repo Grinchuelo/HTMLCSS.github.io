62d92572f65ef269960c0cc52c8c63e232f95d08
This Ban <rulos.trabajando@gmail.com>
2024-06-03 18:47:24 -0300

    Cambios en la clase de inglés

--- code.js
+++ code.js
@@ -1,23 +1,18 @@
 function disableCSS(){
     estilos.href = '';
-    changeToRed();
-}
-function activeCSS(){
-    estilos.href = "style.css";
-    changeToGreen();
-}
-function changeToRed(){
     btnDisabled.style.backgroundColor = "#900";
     btnActivated.style.backgroundColor = "#000";
     btnActivated.style.borderColor = "#000";
     btnDisabled.style.borderColor = "#000";
 }
-function changeToGreen(){
+function activeCSS(){
+    estilos.href = "style.css";
     btnActivated.style.backgroundColor = "#090";
     btnDisabled.style.backgroundColor = "#000";
     btnActivated.style.borderColor = "#fff";
     btnDisabled.style.borderColor = "#fff";
 }
+
 let estilos = document.getElementById('estilos');
 let btnDisabled = document.getElementById('btnDisabled');
 let btnActivated = document.getElementById('btnActivated');
